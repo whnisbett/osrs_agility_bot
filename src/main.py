827f0569ebb007f1cf22424c73a6e7dcaf28c3e1
Billy Nisbett <william.nisbett@gmail.com>
2021-06-03 01:18:52 -0500

    Fix: remove private method markers

--- src/main.py
+++ src/main.py
@@ -14,16 +14,16 @@ class Bot:
         # Initialize detector, have it look for green. If no green, look for red. If found red, look for MoG.
         return
 
-    def _get_rl_screenshot(self):
-        win_handles = self._get_window_handles()
-        rl_handle_id = self._find_rl_handle(win_handles)
+    def get_rl_screenshot(self):
+        win_handles = self.get_window_handles()
+        rl_handle_id = self.find_rl_handle(win_handles)
         win32gui.SetForegroundWindow(rl_handle_id)
         bbox = win32gui.GetWindowRect(rl_handle_id)
         img = ImageGrab.grab(bbox)
         img = np.array(img)
         return img
 
-    def _find_rl_handle(self, win_handles):
+    def find_rl_handle(self, win_handles):
         rl_handle = [
             (handle_id, title)
             for handle_id, title in win_handles
@@ -35,7 +35,7 @@ class Bot:
         rl_handle_id = rl_handle[0]
         return rl_handle_id
 
-    def _get_window_handles(self):
+    def get_window_handles(self):
         win_handles = []
 
         def window_enum_handler(hwnd, results):
@@ -46,33 +46,33 @@ class Bot:
 
         return win_handles
 
-    def _remove_hud(self, img):
-        img = self._remove_border(img)
-        img = self._remove_chatbox(img)
-        img = self._remove_minimap(img)
-        img = self._remove_toolbar(img)
-        img = self._remove_info(img)
+    def remove_hud(self, img):
+        img = self.remove_border(img)
+        img = self.remove_chatbox(img)
+        img = self.remove_minimap(img)
+        img = self.remove_toolbar(img)
+        img = self.remove_info(img)
 
         return img
 
-    def _remove_border(self, img):
+    def remove_border(self, img):
         img[0:30, :, :] = 0
         img[:, 2450:, :] = 0
         return img
 
-    def _remove_chatbox(self, img):
+    def remove_chatbox(self, img):
         img[1240:, :520, :] = 0
         return img
 
-    def _remove_minimap(self, img):
+    def remove_minimap(self, img):
         img[:200, 2240:, :] = 0
         return img
 
-    def _remove_toolbar(self, img):
+    def remove_toolbar(self, img):
         img[1300:, 2020:, :] = 0
         return img
 
-    def _remove_info(self, img):
+    def remove_info(self, img):
         img[:175, :150, :] = 0
         return img
 
@@ -93,18 +93,24 @@ class Detector:
 
     def __call__(self, img):
         self.img = img
-        self._update()
+        self.update()
         return
-    def _get_clickbox_bboxes(self, img):
-        thresh_img = self._preprocess_image(img)
-        bboxes = self._find_shape_bboxes(thresh_img)
-        bboxes = self._remove_extra_bboxes(bboxes)
-        bboxes = self._adjust_bboxes(bboxes)
+
+    def update(self):
+        # is red or is green (change has_mog based on this)
+        #
+
+        return
+    def get_clickbox_bboxes(self, img):
+        thresh_img = self.preprocess_image(img)
+        bboxes = self.find_shape_bboxes(thresh_img)
+        bboxes = self.remove_extra_bboxes(bboxes)
+        bboxes = self.adjust_bboxes(bboxes)
         if len(bboxes) == 0:
             log.warning("No clickboxes detected.")
         return bboxes
 
-    def _preprocess_image(self, img):
+    def preprocess_image(self, img):
         _, thresh_img = cv2.threshold(
             img,
             self.settings["thresh"],
@@ -115,7 +121,7 @@ class Detector:
         img_blur[img_blur.nonzero()] = self.settings["maxval"]
         return img_blur
 
-    def _find_shape_bboxes(self, thresh_img):
+    def find_shape_bboxes(self, thresh_img):
         conts, _ = cv2.findContours(
             thresh_img, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE
         )
@@ -129,7 +135,7 @@ class Detector:
             bboxes.append(bbox)
         return bboxes
 
-    def _remove_extra_bboxes(self, bboxes):
+    def remove_extra_bboxes(self, bboxes):
         bboxes = [
             bbox
             for bbox in bboxes
@@ -137,11 +143,10 @@ class Detector:
         ]
         return bboxes
 
-    def _compute_bbox_area(self, bbox):
+    def compute_bbox_area(self, bbox):
         return bbox[2] * bbox[3]
 
-    def _adjust_bboxes(self, bboxes):
-        adjustment = self.settings["bbox_adjustment"]
+    def adjust_bboxes(self, bboxes):
         adj_bboxes = []
         for bbox in bboxes:
             x, y, l, w = bbox
